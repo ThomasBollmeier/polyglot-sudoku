0a328203b4965a57338ebcbb427c95f2835b7829
Thomas Bollmeier <developer@tbollmeier.org>
2018-03-08 19:09:56 +0100

    Clojure dev. 2

---
+++ clojure/sudoku/src/sudoku/board.clj
@@ -0,0 +1,35 @@
+(ns sudoku.board
+  (:require [sudoku.layout :as layout]))
+
+
+(defn make-board
+  [layout]
+  (let [size (layout/size layout)]
+    {
+      :layout layout
+      :cells (reduce
+               (fn [board [row col]]
+                 (assoc board [row col] (set (range 1 (inc size)))))
+               {}
+               (for [row (range size)
+                     col (range size)]
+                 [row col]))}))
+
+(defn find-free-cell
+  [{:keys [cells]}]
+  (let [free-cells (filter (fn [[pos values]] (> (count values) 1)) cells)]
+    (-> (reduce
+          (fn [{:keys [min-cell min-cnt] :as acc} [_ values :as cell]]
+            (if (or (nil? min-cell) (< (count values) min-cnt))
+              {:min-cell cell
+               :min-cnt (count values)}
+              acc))
+          {:min-cell nil
+           :min-cnt 0}
+        free-cells)
+        :min-cell)))
+
+
+(def b (make-board [2 2]))
+
+(find-free-cell b)
--- clojure/sudoku/src/sudoku/layout.clj
+++ clojure/sudoku/src/sudoku/layout.clj
@@ -1,22 +1,15 @@
 (ns sudoku.layout)
 
+(defn size
+  [[nrows-area ncols-area]]
+  (* nrows-area ncols-area))
+
 (defn cells
   [[nrows-area ncols-area]]
   (let [size (* nrows-area ncols-area)]
     (for [row (range size)
           col (range size)]
       [row col])))
-
-(defn groups
-  [layout]
-  (let [[nrows-area ncols-area] layout
-        size (* nrows-area ncols-area)
-        rows (map (partial row-cells layout) (range size))
-        columns (map (partial column-cells layout) (range size))
-        ;; TODO: ...areas...
-        ]
-    (->> rows
-         (concat columns))))
 
 (defn row-cells
   [[nrows-area ncols-area] row]
@@ -37,3 +30,26 @@
     (for [dc (range ncols-area)
           dr (range nrows-area)]
       [(+ r dr) (+ c dc)])))
+
+(defn groups
+  [[nrows-area ncols-area :as layout]]
+  (let [size (* nrows-area ncols-area)
+        rows (map (partial row-cells layout) (range size))
+        columns (map (partial column-cells layout) (range size))
+        areas (for [x (range nrows-area)
+                    y (range ncols-area)]
+                (area-cells layout x y))]
+    (-> rows
+        (concat columns)
+        (concat areas))))
+
+(defn siblings
+  [[nrows-area ncols-area :as layout] [row col]]
+  (let [x (int (/ col ncols-area))
+        y (int (/ row nrows-area))
+        rcells (row-cells layout row)
+        ccells (column-cells layout col)
+        acells (area-cells layout x y)
+        cells (concat rcells ccells acells)]
+    (-> (set cells)
+        (disj cells [row col]))))
