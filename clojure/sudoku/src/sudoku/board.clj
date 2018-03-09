2578a57554cd2e583152bfae514a1fef3c65c5da
Thomas Bollmeier <developer@tbollmeier.org>
2018-03-09 21:04:47 +0100

    Clojure dev. 3

--- clojure/sudoku/src/sudoku/board.clj
+++ clojure/sudoku/src/sudoku/board.clj
@@ -26,7 +26,43 @@
         free-cells)
         :min-cell)))
 
+(defn remove-value
+  [board cell value]
+  (let [vals (get-in board [:cells cell])]
+    (if (contains? vals value)
+      (let [next-vals (disj vals value)]
+        {:board (assoc-in board [:cells cell] next-vals)
+         :single-val (when (= 1 (count next-vals)) (first next-vals))})
+      {:board board
+       :single-val nil})))
 
-(def b (make-board [2 2]))
+(defn set-value
+  [board cell value]
+  (defn set-val-int
+    [board cell-vals]
+    (if (nil? (seq cell-vals))
+      board
+      (let [[c v] (first cell-vals)
+            siblings (layout/siblings (:layout board) c)
+            {:keys [board cell-vals]} (reduce
+                                        (fn [acc sibling]
+                                          (let [{:keys [board single-val]} (remove-value (:board acc) sibling v)]
+                                            (if (nil? single-val)
+                                              (assoc acc :board board)
+                                              (assoc acc :board board :cell-vals (conj (:cell-vals acc) [sibling single-val])))))
+                                        {:board (assoc-in board [:cells c] #{v})
+                                         :cell-vals (rest cell-vals)}
+                                        siblings)]
+        (recur board cell-vals))))
+  (set-val-int board [[cell value]]))
+
+
+
+(-> (make-board [2 2])
+    (set-value [0 0] 1)
+    (set-value [0 1] 2)
+    (set-value [1 0] 3))
+
+
 
 (find-free-cell b)
