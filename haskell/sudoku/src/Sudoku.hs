846f546f2332e5dc152e0861f409f2ecde73e6b0
Thomas Bollmeier <developer@tbollmeier.org>
2018-05-29 19:57:32 +0200

    Solution algorithm 1.version

--- haskell/sudoku/src/Sudoku.hs
+++ haskell/sudoku/src/Sudoku.hs
@@ -7,11 +7,32 @@ module Sudoku
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Maybe
 
 type Position = (Int, Int)
 type Cells = Map.Map Position Int
 type Layout = (Int, Int) -- (numRowsPerArea, numColumnsPerArea)
-data Board = MkBoard Layout Cells deriving (Show)
+data Board = MkBoard Layout Cells
+
+instance Show Board where
+  show = showBoard
+
+showBoard :: Board -> String
+showBoard b = foldl (\s rowStr -> s ++ rowStr ++ "\n" ) "" rowStrs
+  where
+    rowStrs = map (showRow b) [0..(size-1)]
+    size = getSize b
+
+showRow :: Board -> Int -> String
+showRow b r = foldl (\s c -> s ++ showCell b (r, c)) "" [0..(size-1)]
+  where
+    size = getSize b
+
+showCell :: Board -> Position -> String
+showCell (MkBoard _ cells) p =
+  case Map.lookup p cells of
+    Just value -> show value
+    Nothing -> "_"
 
 getSize :: Board -> Int
 getSize (MkBoard (nRowsArea, nColsArea) _) = nRowsArea * nColsArea
@@ -50,6 +71,17 @@ getSiblings b (r, c) =
     x = c `div` nColsArea
     y = r `div` nRowsArea
 
+getGroups :: Board -> [[Position]]
+getGroups b =
+  [getRow b r | r <- [0..maxIdx]] ++
+  [getColumn b c | c <- [0..maxIdx]] ++
+  [getArea b x y | x <- [0..maxX], y <- [0..maxY]]
+  where
+    (MkBoard (nRowsArea, nColsArea) _) = b
+    maxIdx = nRowsArea * nColsArea - 1
+    maxX = nRowsArea - 1
+    maxY = nColsArea - 1
+
 makeBoard :: Int -> Int -> Board
 makeBoard nRowsArea nColsArea = MkBoard (nRowsArea, nColsArea) Map.empty
 
@@ -61,19 +93,35 @@ setCells :: Board -> [(Position, Int)] -> Board
 setCells b pvs = foldl (\b (p,v) -> setCell b p v) b pvs
 
 solve :: Board -> Maybe Board
-solve b = Nothing -- TODO: implement
+solve b = if not (isValid b)
+  then Nothing
+  else case solutions of
+    Nothing -> Just b -- no free position => we are done
+    Just [] -> Nothing -- no valid alternative => error
+    Just (mb:_) -> mb -- solution found
+  where
+    solutions = do
+      p <- getFreePosition b
+      let vs = getCandidates b p
+      let mbs = map (solve' b p) vs
+      return $ dropWhile (not . maybeIsValid) mbs
+    maybeIsValid = \mb -> case mb of
+      Just board -> isValid board
+      Nothing -> False
 
-consMaybe :: Maybe a -> Maybe [a] -> Maybe [a]
-consMaybe Nothing mxs = mxs
-consMaybe mx mxs = (:) <$> mx <*> mxs
+solve' :: Board -> Position -> Int -> Maybe Board
+solve' b p v =
+  if isValid b'
+    then solve b'
+    else Nothing
+  where
+    b' = setCell b p v
 
 getUsedValues :: Board -> [Position] -> Set.Set Int
 getUsedValues (MkBoard _ cells) ps =
-  case mUsedValues of
-    Just usedValues -> Set.fromList usedValues
-    Nothing -> Set.empty
+  Set.fromList usedValues
     where
-      mUsedValues = foldr consMaybe (Just []) values
+      usedValues = catMaybes values
       values = fmap (\p -> Map.lookup p cells) ps
 
 getCandidates :: Board -> Position -> [Int]
@@ -95,3 +143,13 @@ getFreePosition b =
     allPositions = Set.fromList [(r, c) | r <- [0..maxIdx], c <- [0..maxIdx]]
     fixedPositions = Set.fromList $ Map.keys cells
     freePositions = Set.toList $ Set.difference allPositions fixedPositions
+
+isGroupValid :: Board -> [Position] -> Bool
+isGroupValid b ps = length fixedCells == Set.size fixedValues
+  where
+    (MkBoard _ cells) = b
+    fixedCells = filter (\p -> p `Map.member` cells) ps
+    fixedValues = getUsedValues b ps
+
+isValid :: Board -> Bool
+isValid b = all (isGroupValid b) (getGroups b)
